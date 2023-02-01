c10a89973e14eda0cdcae76e7a8984a8d2d7dec7
MohRana <MoharanaOS@outlook.es>
2023-02-01 17:37:55 -0600

    added screens

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-import { useState } from "react";
-import { View, SafeArea, FlatList, Text } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { NFTCard, HomeHeader, FocusedStatus } from "../components";
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
-const App = () => {
-  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
-};
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- components/index.js
+++
@@ -1,5 +0,0 @@
-import HomeHeader from "./HomeHeader";
-import FocusedStatus from "./FocusedStatus";
-import NFTCard from "./NFTCard";
-
-export { HomeHeader, FocusedStatus, FocusedStatus };
